f84d7da5d0d62165b0753e3b6a434562a230a59b
Migue Baccola <miguelbaccola@hotmail.com>
2023-06-13 20:55:34 -0300

    Ultimo testeo

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testMatch: ["tests/recorded.test.ts"],
+    use: {
+        headless: false,
+        screenshot: "on",
+        video: "on"
+    },
+    reporter: [["dot"], ["json",{
+        outputFile: "jsonReports/jsonReport.json" 
+    }], ["html", {
+        open: "never"
+    }]]
+};
+
+export default config;
